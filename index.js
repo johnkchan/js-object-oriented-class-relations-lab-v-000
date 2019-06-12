42d13ea821a198adf1680bcbaef91e27fa2ba1c6
Jonathan Chan <johnkchan252@gmail.com>
2019-06-12 03:08:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,6 +53,10 @@ class Trip {
   }
   
   passenger() {
-    
+    return store.passengers.find(
+      function(passenger) {
+        return passenger.id === this.passengerId;
+      }.bind(this)
+    );
   }
 }
