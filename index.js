f73d6255d0cfd7c4f0e5d87272fd942b0256c0fe
Jonathan Chan <johnkchan252@gmail.com>
2019-06-12 03:19:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,7 +43,11 @@ class Passenger {
     }
     
     drivers() {
-      
+      return store.drivers.filter(
+        function(driver) {
+          return driver.tripId === this.tripId;
+        }.bind(this)
+      );
     }
 }
 
