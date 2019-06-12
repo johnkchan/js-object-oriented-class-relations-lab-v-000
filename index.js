c43d88670070d3f822f4362d854a85a0ac44ee16
Jonathan Chan <johnkchan252@gmail.com>
2019-06-12 03:02:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,18 +19,18 @@ class Driver {
  
 let passengerId = 0;
 class Passenger {
-    constructor(name, price, user) {
+    constructor(name) {
         this.id = ++itemId;
         this.name = name;
-        this.price = price;
-        if (user) {
-            this.userId = user.id;
-        }
  
-        // insert in the item to the store
         store.items.push(this);
     }
-    setUser(user) {
-        this.userId = user.id;
+    
+    trips() {
+      
+    }
+    
+    passengers() {
+      
     }
 }
